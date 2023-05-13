776629eb8b1b3d3fdded59d69b7842668b966685
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-05-12 20:06:40 -0400

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,26 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import api from './env'
 import { Virtuoso } from 'react-virtuoso'
+import ResizeObserver from 'resize-observer-polyfill'
+
+// To support legacy browsers, you might have to load a ResizeObserver Polyfill before using react-virtuoso:
+if (!window.ResizeObserver)
+  window.ResizeObserver = ResizeObserver
 
 function App() {
   const BASE_URL = `https://api.unsplash.com/photos/random/?client_id=${api}&count=5`
   
   const [photos, setPhotos] = useState([])
 
-  const getPhotos = useCallback(async() => {
+  const loadMore = useCallback(async() => {
     const response = await fetch(BASE_URL)
     const data = await response.json()
     setPhotos((prevState) => [...prevState, ...data])
   }, [BASE_URL, setPhotos])
 
   useEffect(() => {
-    getPhotos().catch(console.error)
-  }, [getPhotos])
+    loadMore().catch(console.error)
+  }, [loadMore])
   
   return (
     <div style={{height:"90lvh"}}>
@@ -29,7 +34,7 @@ function App() {
       <h1>Infinite Scroll</h1>
       <Virtuoso 
         data={photos}
-        endReached={getPhotos}
+        endReached={loadMore}
         overscan={200}
         useWindowScroll
         itemContent={(index, photo) => {
